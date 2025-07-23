3bba3f163fe30821e257c1c1b06f6070b8dc76a5
LuisNunes301 <vnunes30@gmail.com>
2025-07-23 16:28:39 -0300

    add maven project, and depedencies , fix configs

---
+++ backend/meetingroomscheduler/src/main/java/com/meetingroom/meetingroomscheduler/MeetingroomschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.meetingroom.meetingroomscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingroomschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingroomschedulerApplication.class, args);
+	}
+
+}
